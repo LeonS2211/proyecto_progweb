41f4e71ba5ab6b7a98a964fbf05a7d431f2b5a5c
LeonS2211 <lgse1021@gmail.com>
2023-05-27 01:50:17 -0500

    agregando final

--- src/components/FilterNames/FilterNames.jsx
+++ src/components/FilterNames/FilterNames.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Chip from '../ChipL/ChipL.jsx'
 import './style.css'
-
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 
 
@@ -10,11 +11,22 @@ const FilterNames =()=>{
     const baseDatos = JSON.parse(localStorage.getItem('basedatos'))
     const profesores = baseDatos.filter(f => (f.role)=="profesor")
     const [arr, setArr] = useState(profesores)
+    const [visual,setVisual] = useState("Hor")
     const [textBusqueda, setTextBusqueda] = useState("")
 
     const handleCita =({profesor},event)=>{
         event.preventDefault()
         localStorage.setItem('citaVision',JSON.stringify(profesor))
+    }
+
+    const handleClickNom =(event)=>{
+        event.preventDefault()
+        setVisual("Nom")
+    }
+
+    const handleClickHor =(event)=>{
+        event.preventDefault()
+        setVisual("Hor")
     }
 
     return (
@@ -24,15 +36,33 @@ const FilterNames =()=>{
                 value={textBusqueda}
                 onChange={e => setTextBusqueda(e.target.value)}
                 ></Form.Control>
-            <ul className="nobullets">
+                <ButtonGroup aria-label="Basic example">
+                  <Button onClick={handleClickNom} variant="secondary">Por Nombre</Button>
+                  <Button onClick={handleClickHor} variant="secondary">Por Horario</Button>
+                </ButtonGroup>
+                {(visual == "Nom") ?
+                 <ul className="nobullets">
                     { arr.filter(f => (f.nombre).includes(textBusqueda))
                         .map(profesor =>{
                         return (<li key={profesor.nombre} >
                             <Chip nombre={profesor.nombre} curso={profesor.curso} handleCita={(event) => handleCita(event, { profesor })}/>
                             </li>)
                     }) } 
-            </ul>
-        </div>
+                 </ul>
+        
+                :
+                 <div>
+                    <ul className="nobullets">
+                    { arr.filter(f => (f.horarios).includes(textBusqueda))
+                        .map(profesor =>{
+                        return (<li key={profesor.nombre} >
+                            <Chip nombre={profesor.nombre} curso={profesor.curso} handleCita={(event) => handleCita(event, { profesor })}/>
+                            </li>)
+                    }) } 
+                 </ul>
+                 </div>
+                 }
+     </div>
     )
 }
 
